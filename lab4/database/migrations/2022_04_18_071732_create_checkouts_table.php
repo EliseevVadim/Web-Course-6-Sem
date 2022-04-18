8a99eeb966ba9986f6e019e1dbd52381b66c8d90
unknown <eliseevv02@mail.ru>
2022-04-18 19:57:18 +0300

    init

---
+++ lab4/database/migrations/2022_04_18_071732_create_checkouts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCheckoutsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checkouts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('cart_id');
+            $table->unsignedBigInteger('checkout_state_id');
+            $table->timestamps();
+            $table->foreign('cart_id')->references('id')->on('carts');
+            $table->foreign('checkout_state_id')->references('id')->on('checkout_states');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checkouts');
+    }
+}
