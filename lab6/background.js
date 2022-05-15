a3a1782e31c545d0f2b4d709de11879ef6c2745c
unknown <eliseevv02@mail.ru>
2022-05-15 19:37:55 +0300

    remove lab6 directory

--- lab6/background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-    alert('Расширение успешно установлено');
-});
-
-chrome.browserAction.onClicked.addListener(function(activeTab)
-{
-    let newURL = "http://localhost:8080";
-    chrome.tabs.create({ url: newURL });
-});
